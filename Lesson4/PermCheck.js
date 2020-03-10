bbb1ea9dfcb034dd42d61ea6304f14acbed1080f
Caleb Thompson <caleb@webstronomers.com>
2020-03-10 13:17:41 -0700

    Lesson 4 complete

---
+++ Lesson4/PermCheck.js
@@ -0,0 +1,15 @@
+//https://app.codility.com/demo/results/trainingU6TSYQ-DTY/
+
+function solution(A) {
+  var N = A.length;
+  var sum = (N * (N + 1)) / 2;
+  var tap = [];
+  for (var i in A) {
+    sum -= A[i];
+    if (tap[A[i]]) {
+      return 0;
+    }
+    tap[A[i]] = true;
+  }
+  return +(sum == 0);
+}
